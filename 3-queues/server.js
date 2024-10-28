4262c89cd7ab5a45b451a4d729e84b04112a7410
Kobi Miller <kobi@loox.io>
2024-10-28 15:26:35 +0200

    Added cors support for demonstration

--- 3-queues/server.js
+++ 3-queues/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const PubSub = require("./pub-sub");
 
 const app = express();
 const PORT = 5050;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/review/post", (req, res) => {
   const { rating } = req.query;
